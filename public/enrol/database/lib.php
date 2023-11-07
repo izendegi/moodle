f95d48648706df51ed92466a8b55e11cb00673cb
Iñigo Zendegi <izendegi@mondragon.edu>
2023-11-07 10:19:36 +0100

    Initial commit

--- public/enrol/database/lib.php
+++ public/enrol/database/lib.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -964,7 +964,7 @@ class enrol_database_plugin extends enrol_plugin {
         $categoryname_l = strtolower($categoryname);
 
         $localcategoryfield = $this->get_config('localcategoryfield', 'id');
-        $defaultcategory    = $this->get_config('defaultcategory');
+        $defaultcategory = $this->get_config('defaultcategory');
 
         if (null === ($defaultcategory = core_course_category::get($defaultcategory, IGNORE_MISSING, true))) {
             $trace->output("default course category does not exist!", 1);
@@ -992,6 +992,8 @@ class enrol_database_plugin extends enrol_plugin {
         }
         if ($enddate) {
             $sqlfields[] = $enddate;
+        if ($categoryname) {
+            $sqlfields[] = $categoryname_l;
         }
 
         $sql = $this->db_get_sql($table, array(), $sqlfields, true);
@@ -1015,7 +1017,7 @@ class enrol_database_plugin extends enrol_plugin {
                         continue;
                     }
                     $course = new stdClass();
-                    $course->fullname  = $fields[$fullname_l];
+                    $course->fullname = $fields[$fullname_l];
                     $course->shortname = $fields[$shortname_l];
                     $course->idnumber  = $idnumber_l ? $fields[$idnumber_l] : '';
                     $course->summary   = $summary_l ? $fields[$summary_l] : '';
@@ -1573,7 +1575,7 @@ class enrol_database_plugin extends enrol_plugin {
 
         return $sql;
     }
-
+    
     /**
      * Tries to make connection to the external database.
      *
