28e22a0d363a9d8a7cf4e40d5d6cc883bf900f65
Iñigo Zendegi <izendegi@mondragon.edu>
2024-06-19 09:40:57 +0200

    mod_subcourse-github-20240619-MOODLE_404_STABLE

--- mod/subcourse/version.php
+++ mod/subcourse/version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_subcourse';
-$plugin->release = 2023122200;
-$plugin->version = 2023122200;
+$plugin->release = 2024061400;
+$plugin->version = 2024061400;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->requires = 2023042400; // Requires 4.2
-$plugin->supported = [402, 403];
+$plugin->requires = 2024041200; // Requires 4.4.
+$plugin->supported = [404, 404];
