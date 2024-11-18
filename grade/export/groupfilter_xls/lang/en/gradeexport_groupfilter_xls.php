73ed844448350a2c28d49a24372928cf388ff304
Iñigo Zendegi <izendegi@mondragon.edu>
2024-11-18 21:13:16 +0100

    gradeexport_groupfilter_xls_moodle44_2023043002

---
+++ grade/export/groupfilter_xls/lang/en/gradeexport_groupfilter_xls.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+// Project implemented by the &quot;Recovery, Transformation and Resilience Plan.
+// Funded by the European Union - Next GenerationEU&quot;.
+//
+// Produced by the UNIMOODLE University Group: Universities of
+// Valladolid, Complutense de Madrid, UPV/EHU, León, Salamanca,
+// Illes Balears, Valencia, Rey Juan Carlos, La Laguna, Zaragoza, Málaga,
+// Córdoba, Extremadura, Vigo, Las Palmas de Gran Canaria y Burgos.
+
+/**
+ * Strings for component 'gradeexport_groupfilter_xls'.
+ *
+ * @package gradeexport_groupfilter_xls
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @copyright 2023 Proyecto UNIMOODLE {@link https://unimoodle.github.io}
+ * @author UNIMOODLE Group (Coordinator) <direccion.area.estrategia.digital@uva.es>
+ * @author Miguel Gutiérrez (UPCnet) <miguel.gutierrez.jariod@upcnet.es>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['eventgradeexported'] = 'Grades in XLS format exported (including group information)';
+$string['pluginname'] = 'Groupfilter Excel spreadsheet';
+$string['privacy:metadata'] = 'The grade export plugin in XLS format does not store any personal data.';
+$string['timeexported'] = 'Last downloaded from this course';
+$string['groupfilter_txt:publish'] = 'Publish grade export in XLS format (including group information)';
+$string['groupfilter_txt:view'] = 'Use grade export in XLS format';
+
+//Added for grade_export_form
+$string['userfieldsheader'] = 'User Fields';
+$string['userfields_form'] = 'Selection of user fields';
+$string['group'] = 'Groups';
