a1024919311ba7519212b4cdc72866ad4ed0faad
Iñigo Zendegi <izendegi@mondragon.edu>
2024-11-25 08:48:30 +0100

    Backport of MDL-83786 (regressión of MDL-63219)

--- admin/tool/mobile/classes/api.php
+++ admin/tool/mobile/classes/api.php
@@ -321,7 +321,7 @@ class api {
             $settings->tool_mobile_filetypeexclusionlist = get_config('tool_mobile', 'filetypeexclusionlist');
             $custommenuitems = get_config('tool_mobile', 'custommenuitems');
             // If filtering of the primary custom menu is enabled, apply only the string filters.
-            if (!empty($CFG->navfilter && !empty($CFG->stringfilters))) {
+            if (!empty($CFG->navfilter) && !empty($CFG->stringfilters)) {
                 // Apply filters that are enabled for Content and Headings.
                 $filtermanager = filter_manager::instance();
                 $custommenuitems = $filtermanager->filter_string($custommenuitems, \context_system::instance());
