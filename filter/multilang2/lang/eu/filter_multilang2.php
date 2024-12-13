08f0e9ea0e0377426796f8aa6e9df5326cc5e631
Iñigo Zendegi <izendegi@mondragon.edu>
2024-12-13 07:53:37 +0100

    filter_multilang2_moodle45_2024121201

--- filter/multilang2/lang/eu/filter_multilang2.php
+++ filter/multilang2/lang/eu/filter_multilang2.php
@@ -27,41 +27,77 @@ defined('MOODLE_INTERNAL') || die();
 
 $string['filtername'] = 'Eduki eleaniztuna (2.bertsioa)';
 $string['parentlangalwaysen'] = 'Beti erabili guraso-hizkuntzak, \'en\' barne.';
-$string['parentlangbehaviour'] = 'guraso-hizkuntzen portaera';
-$string['parentlangbehaviour_desc'] = '<p>Iragazkiak hizkuntza-bloke
-bat bistaratu behar den ala ez egiaztatzen duenean, blokean
-zehaztutako hizkuntzak edukia ikusten ari den erabiltzailea erabiltzen
-ari den hizkuntzarekin lotzen saiatzen da. Bat-etortze hau hiru era
-ezberdinetan egin daiteke. Iragazkiak hiru era horiei
-<em>guraso-hizkuntzen portaera</em> deitzen die:</p>
-<ul>
-
-<li><b>Beti erabili guraso-hizkuntzak, \'en\' izan ezik</b>. <em>Hau
-da pluginaren portaera tradizionala</em>, eta ezarpenaren balio
-lehenetsia da. Jokabide hau hautatzen denean, iragazkiak
-hizkuntza-blokean zehaztutako hizkuntzak eta hizkuntza horien
-guraso-hizkuntza guztiak (errekurtsiboki hartuta) erabiltzen ditu,
-edukia ikusten ari den erabiltzailea erabiltzen ari den hizkuntzarekin
-bat etortzeko. Baina ingelesa (\'en\') ez da inoiz
-guraso-hizkuntzatzat hartzen kasu honetan, eta guraso-hizkuntzen
-zerrendatik kentzen da. Adibidez, hizkuntza-zerrendan \'en_kids\'
-zehazten duen hizkuntza-bloke bat ez da bistaratuko edukia ikusten are
-den erabiltzailea erabiltzen ari den hizkuntza \'en\' bada.  Hala eta
-guztiz ere, iragazkiak ingelesa (\'en\') nahitaez erabiliko du
-hizkuntza-blokean esplizituki zehazten bada. (adib., <small><tt>{mlang
-en}Testu hau bistaratuko da edukia ikusten ari den erabiltzailea
-erabiltzen ari den hizkuntza \'en\' bada{mlang}</tt></small>)</li>
-<li><b>Beti erabili guraso-hizkuntzak, \'en\' barne </b>. "Beti erabili
-guraso-hizkuntzak, \'en\' izan ezik" bezala funtzionatzen duena, baina
-ez du ingelesa (\'en\') guraso-hizkuntzen zerrendatik kentzen.
-Adibidez, hizkuntza-zerrendan \'en_kids\' zehazten duen
-hizkuntza-bloke bat bistaratuko da edukia ikusten ari den
-erabiltzailea erabiltzen ari den hizkuntza \'en\' bada.</li>
-<li><b>Ez erabili inoiz guraso-hizkuntzak.</b> Izenak dioen bezala,
-guraso-hizkuntzak ez dira inoiz erabiliko edukia ikusten ari den
-erabiltzailea erabiltzen ari den hizkuntzarekin bat egiteko. Iragazkia
-hizkuntza-blokean zehaztutako hizkuntzetara mugatzen da.</li>
-</ul>';
+$string['parentlangbehaviour'] = 'Guraso-hizkuntzen portaera';
+$string['parentlangbehaviour_desc'] = '
+<p>
+  Iragazkiak hizkuntza-bloke bat erakutsi behar den blokean
+  zehaztutako hizkuntzaren eta erabiltzailea une horretan erabiltzen
+  den hizkuntzaren ("erabiltzailearen une horretako hizkuntza") arabera
+  erabakitzen du. Bat-etortze prozesu hori "<em>guraso-hizkuntzen
+  portaera</em>" deitutako hiru modutan egin daiteke:
+</p>
+<ol>
+  <li>
+    <b>Beti erabili guraso-hizkuntzak, \'en\' izan ezik.</b>
+    <ul>
+      <li>
+        <em>Hau da pluginaren portaera tradizionala</em> eta
+        lehenetsitako aukera. Iragazkiak <code>{mlang...}</code>
+        etiketa arteko hizkuntza-blokean zehaztutako hizkuntzak eta
+        euren guraso-hizkuntza guztiak hartuko ditu kontuan, jatorrizko
+        <code>en</code> hizkuntza izan ezik.
+      </li>
+      <li>
+        Adibidez: hizkuntza-bloke batek <code>{mlang en_us_k12}</code>
+        zehazten badu, soilik erakutsiko da erabiltzailearen uneko
+        hizkuntza <code>en_us_k12</code> ala <code>en_us</code> bada,
+        baina ez hizkuntza <code>en</code> bada.
+      </li>
+      <li>
+        Oharra: Ingelesa hizkuntza-blokeetan esplizituki ere erabili
+        daiteke. Esaterako <code>{mlang en}This text will be shown
+        when the user’s current language is \'en\'.{mlang}</code>
+        hizkuntza-blokea erakutsi egingo da erabiltzailearen uneko
+        hizkuntza <code>en</code> denean.
+      </li>
+    </ul>
+  </li>
+  <li>
+    <b>Beti erabili guraso-hizkuntzak, \'en\' barne.</b>
+    <ul>
+      <li>
+        Aukera honek "Beti erabili guraso-hizkuntzak, \'en\' izan ezik"
+        ezarpena bezala funtzionatzen du, baina jatorrizko
+        <code>en</code> hizkuntza baliozko guraso-hizkuntza gisa
+        hartuko da.
+      </li>
+      <li>
+        Adibidez: hizkuntza-bloke batek <code>{mlang en}</code>
+        erabiltzen badu, blokea erakutsi egingo da erabiltzailearen
+        une hizkuntza <code>en_us_k12</code>, <code>en_us</code> ala
+        <code>en</code> denean.
+      </li>
+    </ul>
+  </li>
+  <li>
+    <b>Ez erabili inoiz guraso-hizkuntzak.</b>
+    <ul>
+      <li>
+        Izenak iradokitzen duen moduan, aukera honekin guraso-hizkuntzak
+        ez dira inoiz kontuan hartzen. Iragazkiak soilik
+        hizkuntza-blokean esplizituki erabiltzen diren hizkuntzak
+        hartuko ditu kontuan, inongo guraso-hizkuntzarik kontuan hartu
+        gabe.
+      </li>
+      <li>
+        Adibidez: hizkuntza-bloke batek <code>{mlang en_us_k12}</code>
+        zehazten badu, soilik erakutsiko da erabiltzailearen uneko
+        hizkuntza <code>en_us_k12</code> denean, eta ez
+        <code>en_us</code> ala <code>en</code> bada.
+      </li>
+    </ul>
+  </li>
+</ol>';
 $string['parentlangdefault'] = 'Beti erabili guraso-hizkuntzak, \'en\' izan ezik (portaera tradizionala).';
 $string['parentlangnever'] = 'Ez erabili inoiz guraso-hizkuntzak.';
 $string['pluginname'] = 'Eduki eleaniztunaren Iragazkia (2.bertsioa)';
