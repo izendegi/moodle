7579ec9ad38e8a51ed4bd5460eece5c6ff95ab40
Iñigo Zendegi <izendegi@mondragon.edu>
2025-04-12 19:52:07 +0200

    Plugins

---
+++ lib/editor/tiny/plugins/codepro/lang/ca/tiny_codepro.php
@@ -0,0 +1,50 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tiny CodePro plugin
+ *
+ * @package     tiny_codepro
+ * @copyright   2023 Josep Mulet Pol <pep.mulet@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['autoprettify'] = 'Auto format';
+$string['autoprettify_desc'] = 'Formata automàticament quan l\'editor s\'obri';
+$string['cancel'] = 'Cancel·la';
+$string['codepro:viewplugin'] = 'Veure plugin';
+$string['customelements'] = 'Elements addicionals';
+$string['customelements_def'] = 'Elements HTML addicionals separats per coma';
+$string['decreasefontsize'] = 'Reduir mida font';
+$string['fullscreen'] = 'Pantalla completa';
+$string['helplinktext'] = 'Source code Pro';
+$string['increasefontsize'] = 'Augmentar mida font';
+$string['linewrap'] = 'Trencar línies llargues';
+$string['opendialog'] = 'Obir com a diàleg';
+$string['openpanel'] = 'Obrir com a panell';
+$string['opt_confuser'] = 'Configurable per usuari';
+$string['opt_defdialog'] = 'Per defecte diàleg';
+$string['opt_defpanel'] = 'Per defecte panell';
+$string['plugin'] = 'codePro';
+$string['pluginname'] = 'Source code Pro';
+$string['prettify'] = 'Formata el codi';
+$string['privacy:metadata'] = 'Aquest plugin no desa cap dada associada amb l\'usuari.';
+$string['save'] = 'Desa';
+$string['settings'] = 'Configuració Source codePro';
+$string['synccaret'] = 'Posició cursor';
+$string['synccaret_def'] = 'Sincronitza la posició del cursor entre els editors';
+$string['themes'] = 'Temes clar/obscur';
+$string['uimode'] = 'Mode UI';
+$string['uimode_def'] = 'UI (diàleg/panell) utilitzat per mostrar el codi';
