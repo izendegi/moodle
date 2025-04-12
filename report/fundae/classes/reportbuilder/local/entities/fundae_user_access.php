b61c00e3b1681fca76b19b7f36f787c2a473a8e6
Iñigo Zendegi <izendegi@mondragon.edu>
2025-04-12 19:52:07 +0200

    Plugins

---
+++ report/fundae/classes/reportbuilder/local/entities/fundae_user_access.php
@@ -0,0 +1,115 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+namespace report_fundae\reportbuilder\local\entities;
+
+use coding_exception;
+use lang_string;
+use report_fundae\reportbuilder\local\helpers\fundae_format;
+use core_reportbuilder\local\entities\base;
+use core_reportbuilder\local\report\column;
+
+/**
+ * @package report_fundae
+ * @author 3iPunt <https://www.tresipunt.com/>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright 3iPunt <https://www.tresipunt.com/>
+ */
+class fundae_user_access extends base {
+
+    /**
+     * Database tables that this entity uses and their default aliases
+     *
+     * @return array
+     */
+    protected function get_default_table_aliases(): array {
+        return [
+            'user_lastaccess' => 'ula',
+        ];
+    }
+
+    /**
+     * The default machine-readable name for this entity that will be used in the internal names of the columns/filters
+     *
+     * @return string
+     */
+    protected function get_default_entity_name(): string {
+        return 'user_access';
+    }
+
+    /**
+     * @return base
+     * @throws coding_exception
+     */
+    public function initialise(): base {
+        $columns = $this->get_all_columns();
+        foreach ($columns as $column) {
+            $this->add_column($column);
+        }
+
+        return $this;
+    }
+
+    /**
+     * The default title for this entity in the list of columns/conditions/filters in the report builder
+     *
+     * @return lang_string
+     */
+    protected function get_default_entity_title(): lang_string {
+        return new lang_string('access', 'report_fundae');
+    }
+
+    /**
+     * Returns list of available columns
+     *
+     * @return column[]
+     * @throws coding_exception
+     */
+    protected function get_all_columns(): array {
+        $columns[] = (new column(
+            'firstaccess',
+            new lang_string('firstaccess', 'report_fundae'),
+            $this->get_entity_name()
+        ))
+            ->add_field('u.id', 'userid')
+            ->add_field('c.id', 'courseid')
+            ->set_is_sortable(true)
+            ->add_callback([fundae_format::class, 'firstaccess']);
+
+        $columns[] = (new column(
+            'lastaccess',
+            new lang_string('lastaccess', 'report_fundae'),
+            $this->get_entity_name()
+        ))
+            ->add_joins($this->get_joins())
+            ->add_field("ula.timeaccess")
+            ->set_is_sortable(true)
+            ->add_callback([fundae_format::class, 'userdate'], get_string('strftimedatetimeshort'));
+
+        return $columns;
+    }
+}
