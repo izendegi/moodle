1c9f23979f71f04c6a8ab221b2acaa6a8ba056fa
Iñigo Zendegi <izendegi@mondragon.edu>
2025-04-23 09:03:12 +0200

    report_growth_moodle50_2025042100

--- report/growth/tests/output/renderers_test.php
+++ report/growth/tests/output/renderers_test.php
@@ -26,6 +26,8 @@
 namespace report_growth\output;
 
 use advanced_testcase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
 /**
  * Class report_growth_renderers_testcase
  *
@@ -35,8 +37,12 @@ use advanced_testcase;
  * @copyright eWallah (www.eWallah.net)
  * @author    Renaat Debleu <info@eWallah.net>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * #[CoversClass(report_growth)]
+ * @covers \report_growth)]
  */
+#[CoversClass(global_renderer::class)]
+#[CoversClass(growth_renderer::class)]
+#[CoversClass(category_renderer::class)]
+#[CoversClass(course_renderer::class)]
 final class renderers_test extends advanced_testcase {
     /** @var int courseid */
     private int $courseid;
@@ -68,8 +74,6 @@ final class renderers_test extends advanced_testcase {
 
     /**
      * Test course tables.
-     * #[CoversClass(report_growth\output\course_renderer)]
-     * #[CoversClass(report_growth\output\growth_renderer)]
      */
     public function test_course(): void {
         global $PAGE;
@@ -85,8 +89,6 @@ final class renderers_test extends advanced_testcase {
 
     /**
      * Test global tables.
-     * #[CoversClass(report_growth\output\global_renderer)]
-     * #[CoversClass(report_growth\output\growth_renderer)]
      */
     public function test_global(): void {
         global $PAGE;
@@ -106,8 +108,6 @@ final class renderers_test extends advanced_testcase {
 
     /**
      * Test category tables.
-     * #[CoversClass(report_growth\output\category_renderer)]
-     * #[CoversClass(report_growth\output\growth_renderer)]
      */
     public function test_category(): void {
         global $PAGE;
