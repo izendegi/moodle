af66da504b4d12e3695dbc797767bf63499879ab
Iñigo Zendegi <izendegi@mondragon.edu>
2025-04-29 10:00:08 +0200

    quiz_answersheets_moodle45_2025040200

--- mod/quiz/report/answersheets/tests/behat/behat_quiz_answersheets.php
+++ mod/quiz/report/answersheets/tests/behat/behat_quiz_answersheets.php
@@ -63,8 +63,6 @@ class behat_quiz_answersheets extends behat_question_base {
      * @throws Exception with a meaningful error message if the specified page cannot be found.
      */
     protected function resolve_page_instance_url(string $type, string $identifier): moodle_url {
-        global $DB;
-
         switch ($type) {
             case 'Report':
                 return new moodle_url('/mod/quiz/report.php',
@@ -83,7 +81,7 @@ class behat_quiz_answersheets extends behat_question_base {
      */
     protected function get_quiz_by_name(string $name): stdClass {
         global $DB;
-        return $DB->get_record('quiz', array('name' => $name), '*', MUST_EXIST);
+        return $DB->get_record('quiz', ['name' => $name], '*', MUST_EXIST);
     }
 
     /**
@@ -95,18 +93,5 @@ class behat_quiz_answersheets extends behat_question_base {
     protected function get_cm_by_quiz_name(string $name): stdClass {
         $quiz = $this->get_quiz_by_name($name);
         return get_coursemodule_from_instance('quiz', $quiz->id, $quiz->course);
-    }
-
-    /**
-     * Check that the given Question type already installed.
-     *
-     * @Given /^I check the "(?P<question_type_string>(?:[^"]|\\")*)" question type already installed for export attempts report$/
-     */
-    public function check_question_type_installed($questiontype) {
-        $qtypes = question_bank::get_creatable_qtypes();
-        if (!array_key_exists($questiontype, $qtypes)) {
-            // Question type not available.
-            throw new \Moodle\BehatExtension\Exception\SkippedException();
-        }
     }
 }
