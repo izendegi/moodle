d464cb54dd3e67b255a2acbdf001b46bdd2409ad
Iñigo Zendegi <izendegi@mondragon.edu>
2025-05-20 08:02:56 +0200

    qtype_ddmatch_moodle50_2025051900_fixrequiredversion

--- question/type/ddmatch/version.php
+++ question/type/ddmatch/version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_ddmatch';
 $plugin->version = 2025051900;
-$plugin->requires = 2024042210;
+$plugin->requires = 2024042208;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'qtype_match' => 2022080900,
