f4996cf00f4d65236ca9edfafea8aca1655fe2df
Iñigo Zendegi <izendegi@mondragon.edu>
2025-05-30 10:53:22 +0200

    Hotfix SQL performance issue

--- question/bank/managecategories/classes/helper.php
+++ question/bank/managecategories/classes/helper.php
@@ -323,10 +323,11 @@ class helper {
                         $statuscondition
                             AND c.id = qbe.questioncategoryid
                             AND ($showallversions = 1
-                                OR (qv.version = (SELECT MAX(v.version)
-                                                    FROM {question_versions} v
-                                                    JOIN {question_bank_entries} be ON be.id = v.questionbankentryid
-                                                   WHERE be.id = qbe.id AND v.status <> :substatus)
+                                OR ((qv.questionbankentryid,qv.version) in ( SELECT be.id, MAX(v.version)
+                                                                               FROM {question_versions} v
+                                                                               JOIN {question_bank_entries} be ON be.id = v.questionbankentryid
+                                                                              WHERE v.status <> :substatus
+                                                                              GROUP BY be.id)
                                    )
                                 )
                             ) AS questioncount
