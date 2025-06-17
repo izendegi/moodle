67a581e10f12f9f090abdbeebc26061b9d9ff4a3
Iñigo Zendegi <izendegi@mondragon.edu>
2025-06-17 08:58:26 +0200

    mod_pdfprotect_moodle50_2025061601

--- mod/pdfprotect/version.php
+++ mod/pdfprotect/version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025042400;
-$plugin->requires = 2019052000;
-$plugin->release = '3.2.3';
+$plugin->version = 2025061601;
+$plugin->requires = 2021041900;
+$plugin->release = '3.2.5';
 $plugin->component = 'mod_pdfprotect';
 $plugin->maturity = MATURITY_STABLE;
