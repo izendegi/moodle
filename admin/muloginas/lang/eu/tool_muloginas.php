e3671ea87937b835530738f5fe0400e1a790a1e0
Iñigo Zendegi <izendegi@mondragon.edu>
2025-06-19 12:59:46 +0200

    Plugins
    tool_deletemessage_moodle50_2025072600
    
    tool_amoslink_moodle44_2024070100
    
    Plugins

---
+++ admin/muloginas/lang/eu/tool_muloginas.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of MuTMS suite of plugins for Moodle™ LMS.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// phpcs:disable moodle.Files.BoilerplateComment.CommentEndedTooSoon
+
+/**
+ * Alternative Log-in-as plugin.
+ *
+ * @package     tool_muloginas
+ * @copyright   2025 Petr Skoda
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['creatingtoken'] = 'Saioa honela hasteko tokena prestatzen...';
+$string['error_incognitoproglem'] = 'Errorea "Hasi saioa honela" saioa habiaraztekoan, ziurtatu ezazu leiho probatu guztiak itxita dituzula eta saiatu zaitez berriro.';
+$string['error_invalidrequest'] = '"Hasi saioa honela" eskaera baliogabea antzeman da.';
+$string['error_linkexpired'] = '"Hasi saioa honela" esteka iraungi da, berriz saiatu beharrezkoa bada.';
+$string['error_notincognito'] = '"Hasi saioa honela" esteka ez da leiho pribatu berri batean ireki.';
+$string['info_heading'] = 'Hasi saioa honela hemen: {$a}';
+$string['loggedinas'] = 'Orain "{$a}" erabiltzailea bazina bezala hasi duzu saioa, amaitzerakoan amaitu saioa ala itxi leiho hau.';
+$string['loggedoutprev'] = 'Lehenagoko "{$a}" erabiltzailearentzako "Hasi saioa honela" saioa amaitu da.';
+$string['loginas'] = 'Hasi saioa honela (Leiho pribatu berriaren bidez)';
+$string['loginas_a'] = 'Hasi saioa "{$a}" gisa';
+$string['logoutinfo'] = '"Hasi saioa honela" saioa amaitu da, itxi leiho hau jarraitzeko.';
+$string['muloginas:loginas'] = 'Hasi saioa honela Leiho pribatu berriaren bidez';
+$string['pluginname'] = 'Hasi saioa honela Leiho pribatu berriaren bidez';
+$string['popup_link'] = 'Egin klik saguaren eskuineko botoiarekin esteka honetan eta aukeratu <strong>Ireki esteka leiho pribatu batean</strong> edo antzeko aukeran.';
+$string['privacy:metadata'] = 'Hasi saioa honela pluginak ez du datu pertsonalik biltzen';
+$string['privacy:metadata:targetuserid'] = 'Helburuko erabiltzailearen IDa';
+$string['privacy:metadata:tool_muloginas_request:tableexplanation'] = 'Hasi-saioa-honela eskaera tokenak';
+$string['privacy:metadata:userid'] = 'Erabiltzailearen IDa';
+$string['taskcron'] = 'Hasi-saioa-honela tokenak garbitzeko ataza';
