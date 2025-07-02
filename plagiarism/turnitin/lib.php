cbf0e01f11458ca4a6c6da3bdc94c227619dbd69
Iñigo Zendegi <izendegi@mondragon.edu>
2025-07-02 15:03:54 +0200

    plagiarism_turnitin_moodle50_2025070201

--- plagiarism/turnitin/lib.php
+++ plagiarism/turnitin/lib.php
@@ -438,8 +438,6 @@ class plagiarism_plugin_turnitin extends plagiarism_plugin {
     public function print_disclosure($cmid) {
         global $OUTPUT, $PAGE, $USER, $DB, $CFG;
 
-        static $tiiconnection;
-
         $config = $this->plagiarism_turnitin_admin_config();
         $output = '';
 
@@ -481,6 +479,46 @@ class plagiarism_plugin_turnitin extends plagiarism_plugin {
         if (!$this->is_plugin_configured()) {
             return $output;
         }
+
+        // Add eula links if necessary
+        $output .= $this->render_eula_form($cm);
+
+        if ($config->plagiarism_turnitin_usegrademark && !empty($plagiarismsettings["plagiarism_rubric"])) {
+
+            // Update assignment in case rubric is not stored in Turnitin yet.
+            $coursedata = $this->get_course_data($cm->id, $cm->course);
+            $this->sync_tii_assignment($cm, $coursedata->turnitin_cid);
+
+            if ($CFG->version >= 2023100900) {
+                $PAGE->requires->js_call_amd('plagiarism_turnitin/new_rubric', 'newRubric');
+            } else {
+                // TODO: We can remove this when we no longer have to support Moodle versions 4.3 and below
+                $PAGE->requires->js_call_amd('plagiarism_turnitin/rubric', 'rubric');
+            }
+
+            $rubricviewlink = html_writer::tag('span',
+                get_string('launchrubricview', 'plagiarism_turnitin'),
+                array('class' => 'rubric_view rubric_view_pp_launch_upload tii_tooltip',
+                    'data-courseid' => $cm->course,
+                    'data-cmid' => $cm->id,
+                    'title' => get_string('launchrubricview',
+                        'plagiarism_turnitin'), 'id' => 'rubric_manager_form'
+                )
+            );
+            $rubricviewlink = html_writer::tag('div', $rubricviewlink, array('class' => 'row_rubric_view'));
+
+            $output .= html_writer::tag('div', $rubricviewlink, array('class' => 'tii_links_container tii_disclosure_links'));
+        }
+
+        return $output;
+    }
+
+    public function render_eula_form($cm) {
+        global $OUTPUT, $USER;
+
+        $output = '';
+
+        static $tiiconnection;
 
         // Show EULA if necessary and we have a connection to Turnitin.
         if (empty($tiiconnection)) {
@@ -529,32 +567,6 @@ class plagiarism_plugin_turnitin extends plagiarism_plugin {
                                                         'POST', $target = 'eulaWindow', array('id' => 'eula_launch'));
                 $output .= $OUTPUT->box($eulaform->display(), 'tii_useragreement_form', 'useragreement_form');
             }
-        }
-
-        if ($config->plagiarism_turnitin_usegrademark && !empty($plagiarismsettings["plagiarism_rubric"])) {
-
-            // Update assignment in case rubric is not stored in Turnitin yet.
-            $this->sync_tii_assignment($cm, $coursedata->turnitin_cid);
-
-            if ($CFG->version >= 2023100900) {
-                $PAGE->requires->js_call_amd('plagiarism_turnitin/new_rubric', 'newRubric');
-            } else {
-                // TODO: We can remove this when we no longer have to support Moodle versions 4.3 and below
-                $PAGE->requires->js_call_amd('plagiarism_turnitin/rubric', 'rubric');
-            }
-
-            $rubricviewlink = html_writer::tag('span',
-                get_string('launchrubricview', 'plagiarism_turnitin'),
-                array('class' => 'rubric_view rubric_view_pp_launch_upload tii_tooltip',
-                    'data-courseid' => $cm->course,
-                    'data-cmid' => $cm->id,
-                    'title' => get_string('launchrubricview',
-                        'plagiarism_turnitin'), 'id' => 'rubric_manager_form'
-                )
-            );
-            $rubricviewlink = html_writer::tag('div', $rubricviewlink, array('class' => 'row_rubric_view'));
-
-            $output .= html_writer::tag('div', $rubricviewlink, array('class' => 'tii_links_container tii_disclosure_links'));
         }
 
         return $output;
@@ -790,9 +802,13 @@ class plagiarism_plugin_turnitin extends plagiarism_plugin {
             $moodlesubmission = $DB->get_record('assign_submission', array('id' => $itemid), 'id, groupid');
 
             if ((!empty($moodlesubmission->groupid)) && ($cm->modname == "assign")) {
-                $plagiarismfiles = $DB->get_records('plagiarism_turnitin_files', ['itemid' => $itemid, 'cm' => $cm->id,
-                    'identifier' => $identifier],
-                    'lastmodified DESC', '*', 0, 1);
+                $plagiarismfiles = $DB->get_records_sql(
+                    'SELECT * FROM {plagiarism_turnitin_files} 
+                     WHERE identifier IN ( :identifier, :oldidentifier)
+                     AND itemid = :itemid AND cm = :cmid 
+                     ORDER BY lastmodified DESC',
+                    ['identifier' => $identifier, 'oldidentifier' => $oldidentifier,
+                     'itemid' => $itemid, 'cmid' => $cm->id], 0, 1);
                 $plagiarismfile = reset($plagiarismfiles);
                 $author = $plagiarismfile->userid ?? null;
                 $linkarray['userid'] = $author;
@@ -900,9 +916,13 @@ class plagiarism_plugin_turnitin extends plagiarism_plugin {
 
                 // Get turnitin file details.
                 if (is_null($plagiarismfile)) {
-                    $plagiarismfiles = $DB->get_records('plagiarism_turnitin_files', array('userid' => $linkarray["userid"],
-                            'cm' => $linkarray["cmid"], 'identifier' => $identifier),
-                            'lastmodified DESC', '*', 0, 1);
+                    $plagiarismfiles = $DB->get_records_sql(
+                        'SELECT * FROM {plagiarism_turnitin_files} 
+                        WHERE identifier IN ( :identifier, :oldidentifier)
+                        AND userid = :userid AND cm = :cmid 
+                        ORDER BY lastmodified DESC',
+                        ['identifier' => $identifier, 'oldidentifier' => $oldidentifier,
+                        'userid' => $linkarray["userid"], 'cmid' => $cm->id], 0, 1);
                     $plagiarismfile = current($plagiarismfiles);
                 }
 
