3bcecc64087793e59261c1e37dfbc7820ba72a4b
Iñigo Zendegi <izendegi@mondragon.edu>
2025-07-04 07:26:18 +0200

    format_tiles_moodle50_2025070360_ibaipatchv3byIZ

--- course/format/tiles/version.php
+++ course/format/tiles/version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025060269;     // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2025070360;     // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2025040800;    // Moodle 5.0.
 $plugin->component = 'format_tiles';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "5.0.0.19";
+$plugin->release = "5.0.0.20";
 $plugin->supported = [500, 500]; // Supported Moodle version range.
