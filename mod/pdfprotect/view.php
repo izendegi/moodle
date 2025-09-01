119018b6fa97ac3edc25a0e6268831f65bd2fe32
Iñigo Zendegi <izendegi@mondragon.edu>
2025-09-01 11:18:38 +0200

    mod_pdfprotect_moodle50_2025082903

--- mod/pdfprotect/view.php
+++ mod/pdfprotect/view.php
@@ -18,7 +18,7 @@
  * Pdfprotect module version information
  *
  * @package   mod_pdfprotect
- * @copyright 2025 Eduardo kraus (http://eduardokraus.com)
+ * @copyright 2025 Eduardo Kraus {@link https://eduardokraus.com}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -29,30 +29,44 @@ $id = optional_param('id', 0, PARAM_INT); // Course Module ID.
 $r = optional_param('r', 0, PARAM_INT);  // Pdfprotect instance ID.
 
 if ($r) {
-    if (!$pdfprotect = $DB->get_record('pdfprotect', [ 'id' => $r ])) {
+    if (!$pdfprotect = $DB->get_record('pdfprotect', ['id' => $r])) {
         throw new Exception('invalidaccessparameter');
     }
     $cm = get_coursemodule_from_instance('pdfprotect', $pdfprotect->id, $pdfprotect->course, false, MUST_EXIST);
-
 } else {
     if (!$cm = get_coursemodule_from_id('pdfprotect', $id)) {
         throw new Exception('invalidcoursemodule');
     }
-    $pdfprotect = $DB->get_record('pdfprotect', [ 'id' => $cm->instance ], '*', MUST_EXIST);
+    $pdfprotect = $DB->get_record('pdfprotect', ['id' => $cm->instance], '*', MUST_EXIST);
 }
 
-$course = $DB->get_record('course', [ 'id' => $cm->course ], '*', MUST_EXIST);
+$course = $DB->get_record('course', ['id' => $cm->course], '*', MUST_EXIST);
 
-require_course_login($course, true, $cm);
+if ($token = optional_param("token", false, PARAM_TEXT)) {
+    $externalservice = $DB->get_record("external_services", ["shortname" => MOODLE_OFFICIAL_MOBILE_SERVICE]);
+    $externaltoken = $DB->get_record("external_tokens", ["token" => $token, "externalserviceid" => $externalservice->id], "userid");
+    $user = $DB->get_record("user", ["id" => $externaltoken->userid]);
+
+    if ($user) {
+        \core\session\manager::login_user($user);
+        $PAGE->set_pagelayout("embedded");
+        $PAGE->add_body_class("body-pdfprotect-mobile-view");
+    }
+    require_course_login($course, false, null, false, true);
+} else {
+    require_course_login($course, true, $cm);
+}
 $context = context_module::instance($cm->id);
 require_capability('mod/pdfprotect:view', $context);
 
 // Completion and trigger events.
 pdfprotect_view($pdfprotect, $course, $cm, $context);
 
-$PAGE->set_url('/mod/pdfprotect/view.php', [ 'id' => $cm->id ]);
-$PAGE->set_title($course->shortname . ': ' . $pdfprotect->name);
+$PAGE->set_url('/mod/pdfprotect/view.php', ['id' => $cm->id]);
+$PAGE->set_title(format_string($pdfprotect->name));
 $PAGE->set_heading($course->fullname);
+$PAGE->set_cm($cm, $course);
+ 
 echo $OUTPUT->header();
 
 echo $OUTPUT->render_from_template('mod_pdfprotect/view_page', ['id' => $id]);
