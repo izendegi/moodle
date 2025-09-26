d8b039daba35957245c3601ed4a3c6606a879620
Iñigo Zendegi Urzelai <izendegi@mondragon.edu>
2025-09-26 15:46:57 +0200

    Fix proposed on MDL-85184
    Adding here the fix proposed by Daniel Ureña on MDL-85184

--- admin/tool/mobile/classes/api.php
+++ admin/tool/mobile/classes/api.php
@@ -323,7 +323,7 @@ class api {
             // If filtering of the primary custom menu is enabled, apply only the string filters.
             if (!empty($CFG->navfilter) && !empty($CFG->stringfilters)) {
                 // Apply filters that are enabled for Content and Headings.
-                $filtermanager = filter_manager::instance();
+                $filtermanager = \filter_manager::instance();
                 $custommenuitems = $filtermanager->filter_string($custommenuitems, \context_system::instance());
             }
             $settings->tool_mobile_custommenuitems = $custommenuitems;
