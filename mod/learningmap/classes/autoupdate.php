1d4383c36882420892aaade3179f3e31f82a5aec
Iñigo Zendegi <izendegi@mondragon.edu>
2025-10-13 07:09:57 +0200

    PLugin update

--- mod/learningmap/classes/autoupdate.php
+++ mod/learningmap/classes/autoupdate.php
@@ -112,7 +112,11 @@ class autoupdate {
         $data = $event->get_data();
         if (isset($data['courseid']) && $data['courseid'] > 0) {
             if (
-                ($data['objecttable'] == 'course' && in_array('format', $data['other']['updatedfields'])) ||
+                (
+                    $data['objecttable'] == 'course' &&
+                    is_array($data['other']['updatedfields']) &&
+                    in_array('format', $data['other']['updatedfields'])
+                ) ||
                 ($data['objecttable'] == 'course_modules' && $data['other']['modulename'] == 'learningmap')
             ) {
                 cachemanager::reset_backlink_cache($data['courseid']);
