13370539adc4c05487263d70d643a28c18c136b7
Iñigo Zendegi <izendegi@mondragon.edu>
2025-10-13 07:09:57 +0200

    PLugin update

--- mod/verbalfeedback/classes/helper.php
+++ mod/verbalfeedback/classes/helper.php
@@ -90,7 +90,8 @@ class helper {
         /** @var instance_category $category */
         foreach ($categories as $category) {
             $categoryviewmodel = new stdClass();
-            $categoryviewmodel->header = $category->get_header($currentlanguage)->get_string();
+            $categoryviewmodel->header = $category->get_header($currentlanguage)
+                ? $category->get_header($currentlanguage)->get_string() : '';
             $categoryviewmodel->id = $category->get_id();
             $categoryviewmodel->criteria = [];
             $categoryviewmodel->position = $category->get_position();
@@ -102,18 +103,25 @@ class helper {
                 $criterionviewmodel->id = $criterion->get_id();
                 $criterionviewmodel->position = $criterion->get_position();
                 $criterionviewmodel->weight = number_format($criterion->get_weight(), 2);
-                $criterionviewmodel->text = $criterion->get_description($currentlanguage)->get_string();
+                $criterionviewmodel->text = $criterion->get_description($currentlanguage)
+                    ? $criterion->get_description($currentlanguage)->get_string() : '';
 
                 foreach ($criterion->get_subratings() as $subrating) {
                     $subratingviewmodel = new stdClass();
 
                     $subratingviewmodel->id = $subrating->get_id();
-                    $subratingviewmodel->title = $subrating->get_title($currentlanguage)->get_string();
-                    $subratingviewmodel->description = $subrating->get_description($currentlanguage)->get_string();
-                    $subratingviewmodel->verynegative = $subrating->get_verynegative($currentlanguage)->get_string();
-                    $subratingviewmodel->negative = $subrating->get_negative($currentlanguage)->get_string();
-                    $subratingviewmodel->positive = $subrating->get_positive($currentlanguage)->get_string();
-                    $subratingviewmodel->verypositive = $subrating->get_verypositive($currentlanguage)->get_string();
+                    $subratingviewmodel->title = $subrating->get_title($currentlanguage)
+                        ? $subrating->get_title($currentlanguage)->get_string() : '';
+                    $subratingviewmodel->description = $subrating->get_description($currentlanguage)
+                        ? $subrating->get_description($currentlanguage)->get_string() : '';
+                    $subratingviewmodel->verynegative = $subrating->get_verynegative($currentlanguage)
+                        ? $subrating->get_verynegative($currentlanguage)->get_string() : '';
+                    $subratingviewmodel->negative = $subrating->get_negative($currentlanguage)
+                        ? $subrating->get_negative($currentlanguage)->get_string() : '';
+                    $subratingviewmodel->positive = $subrating->get_positive($currentlanguage)
+                        ? $subrating->get_positive($currentlanguage)->get_string() : '';
+                    $subratingviewmodel->verypositive = $subrating->get_verypositive($currentlanguage)
+                        ? $subrating->get_verypositive($currentlanguage)->get_string() : '';
 
                     $criterionviewmodel->subratings[] = $subratingviewmodel;
                 }
@@ -137,10 +145,9 @@ class helper {
      */
     public static function parseyamlfile(string $someyaml, ?int $options = null, ?int $debug = null) {
         if (version_compare(PHP_VERSION, '8.1.14') >= 0) {
-            require_once(implode(DIRECTORY_SEPARATOR, [dirname(__FILE__), 'vendor', '81x', 'autoload.php']));
+            require_once(implode(DIRECTORY_SEPARATOR, [dirname(__FILE__), 'vendor', 'autoload.php']));
             return \Dallgoot\Yaml\Yaml::parseFile($someyaml, $options, $debug);
         }
-        require_once(implode(DIRECTORY_SEPARATOR, [dirname(__FILE__), 'vendor', '74x', 'autoload.php']));
-        return \Dallgoot\Yaml::parseFile($someyaml, $options, $debug);
+        throw new Exception('YAML parsing requires at least PHP 8.1.14');
     }
 }
