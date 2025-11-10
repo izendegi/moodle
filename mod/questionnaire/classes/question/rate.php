a81f150e0d03bcd298e3b56fb0bcecdc4b1343b0
Iñigo Zendegi <izendegi@mondragon.edu>
2025-11-10 14:23:57 +0100

    mod_questionnaire_moodle51_2025041400_filterposition_fixmlang

--- mod/questionnaire/classes/question/rate.php
+++ mod/questionnaire/classes/question/rate.php
@@ -330,7 +330,8 @@ class rate extends question {
             $cols = [];
             if (isset($choice->content)) {
                 $str = 'q' . "{$this->id}_$cid";
-                $content = $choice->content;
+                //$content = $choice->content;
+                $content = format_text($choice->content, FORMAT_HTML, ['noclean' => true]);
                 $rendercontent = format_text($choice->content, FORMAT_PLAIN);
                 if ($this->osgood_rate_scale()) {
                     [$content, $contentright] = array_merge(preg_split('/[|]/', $content), [' ']);
@@ -344,6 +345,7 @@ class rate extends question {
                         $ovalue = stripslashes($odata);
                     }
                     $content = $othertext;
+                    //$content = format_text($othertext, FORMAT_HTML, ['noclean' => true]);
                     $cols[] = ['oname' => $oname, 'oid' => $oid, 'ovalue' => $ovalue,
                             'colstyle' => 'text-align: ' . $textalign . ';',
                             'coltext' => format_text($content, FORMAT_HTML, ['noclean' => true]) . '&nbsp;'];
@@ -528,13 +530,15 @@ class rate extends question {
                 $content = $choice->content;
                 $contents = questionnaire_choice_values($content);
                 if ($contents->modname) {
-                    $content = $contents->text;
+                    //$content = $contents->text;
+                    $content = format_text($contents->text, FORMAT_HTML, ['noclean' => true]);
                 }
                 if ($this->osgood_rate_scale()) {
                     [$content, $contentright] = array_merge(preg_split('/[|]/', $content), [' ']);
                 }
                 if ($choice->is_other_choice()) {
                     $content = $choice->other_choice_display();
+                    //$content = format_text($choice->other_choice_display(), FORMAT_HTML, ['noclean' => true]);
                     if (isset($response->answers[$this->id][$cid]->otheresponse)) {
                         $rowobj->othercontent = $response->answers[$this->id][$cid]->otheresponse;
                     }
@@ -1017,7 +1021,13 @@ class rate extends question {
      */
     private function add_nameddegrees_from_extradata() {
         if (!empty($this->extradata)) {
-            $this->nameddegrees = json_decode($this->extradata, true);
+            //$this->nameddegrees = json_decode($this->extradata, true);
+            $degrees = json_decode($this->extradata, true);
+            if (is_array($degrees)) {
+                foreach ($degrees as $key => $value) {
+                    $this->nameddegrees[$key] = format_text($value, FORMAT_HTML, ['noclean' => true]);
+                }
+            }
         }
     }
 
