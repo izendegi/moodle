dd82b2f8c5a7093de66702c3bee8b407cf9563a0
Iñigo Zendegi <izendegi@mondragon.edu>
2025-11-18 16:45:22 +0100

    mod_learningmap_moodle51_2025111800

--- mod/learningmap/version.php
+++ mod/learningmap/version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_learningmap';
-$plugin->release = '0.9.13';
-$plugin->version = 2025092600;
+$plugin->release = '0.9.14';
+$plugin->version = 2025111800;
 $plugin->requires = 2022112800;
-$plugin->supported = [401, 501];
+$plugin->supported = [405, 501];
 $plugin->maturity = MATURITY_STABLE;
