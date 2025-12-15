144b99b41767d6d2129c8fcf1012d5d595250821
Iñigo Zendegi <izendegi@mondragon.edu>
2025-12-15 08:46:27 +0100

    mod_journal_moodle51_2025121400

--- mod/journal/backup/moodle2/backup_journal_stepslib.php
+++ mod/journal/backup/moodle2/backup_journal_stepslib.php
@@ -30,22 +30,37 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class backup_journal_activity_structure_step extends backup_activity_structure_step {
-
     /**
      * Define the structure
      *
      * @return void
      */
     protected function define_structure() {
-
         $journal = new backup_nested_element('journal', ['id'], [
-            'name', 'intro', 'introformat', 'days', 'grade', 'timemodified', ]);
+            'name',
+            'intro',
+            'introformat',
+            'days',
+            'grade',
+            'timemodified',
+            'completion_create_entry',
+            'notifyteachers',
+            'notifystudents',
+        ]);
 
         $entries = new backup_nested_element('entries');
 
         $entry = new backup_nested_element('entry', ['id'], [
-            'userid', 'modified', 'text', 'format', 'rating',
-            'entrycomment', 'teacher', 'timemarked', 'mailed', ]);
+            'userid',
+            'modified',
+            'text',
+            'format',
+            'rating',
+            'entrycomment',
+            'teacher',
+            'timemarked',
+            'mailed',
+        ]);
 
         // Journal -> entries -> entry.
         $journal->add_child($entries);
