eecc94b21dc16b13467dc5031fc4d40a16a13ace
Iñigo Zendegi <izendegi@mondragon.edu>
2026-01-08 06:59:36 +0100

    mod_verbalfeedback_moodle51_2026010500__templatemlang

--- mod/verbalfeedback/version.php
+++ mod/verbalfeedback/version.php
@@ -25,9 +25,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_verbalfeedback';
-$plugin->version = 2025120100;
+$plugin->version = 2026010500;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron      = 0;
 $plugin->supported = [401, 501];
-$plugin->release   = 'v5.1-r2';
+$plugin->release   = 'v5.1-r3';
