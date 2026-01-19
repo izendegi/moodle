1b1fbb7c1e5fb9c01d4a7099f7fb783d99e86cf5
Iñigo Zendegi <izendegi@mondragon.edu>
2026-01-19 08:54:39 +0100

    mod_journal_moodle51_2026011801

--- mod/journal/version.php
+++ mod/journal/version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_journal';
-$plugin->version = 2025121400;
+$plugin->version = 2026011801;
 $plugin->requires = 2022041900;  /* Moodle 4.0 */
-$plugin->release = '5.0.0 (Build: 2025121400)';
+$plugin->release = '5.1.1 (Build: 2026011801)';
 $plugin->maturity = MATURITY_STABLE;
