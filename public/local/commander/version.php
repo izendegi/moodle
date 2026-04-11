30167c1e30d029febf7f2be8ef9b6bcd00d7e0ef
Iñigo Zendegi <izendegi@mondragon.edu>
2026-04-11 09:23:55 +0200

    local_commander_moodle51_2026040100

--- public/local/commander/version.php
+++ public/local/commander/version.php
@@ -25,9 +25,9 @@
  **/
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release = '5.0.0';
+$plugin->release = '5.1.0';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->version = 2025052100;
-$plugin->requires = 2015111602;
+$plugin->version = 2026040100;
+$plugin->requires = 2022041900;
 $plugin->component = 'local_commander';
-$plugin->supported = [39, 500];
+$plugin->supported = [401, 501];
