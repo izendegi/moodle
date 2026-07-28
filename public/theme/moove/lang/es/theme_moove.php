a5013e8c50cb1ec0e6a2bb3a28f796c423ca1be0
Iñigo Zendegi Urzelai <izendegi@mondragon.edu>
2026-07-28 10:11:44 +0200

    theme_moove-5.2.32-20260728_loginpagedisplay

---
+++ public/theme/moove/lang/es/theme_moove.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file.
+ *
+ * @copyright 2017 Willian Mano - http://conecti.me
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// Custom login page
+$string['loginmu'] = 'Mondragon Unibertsitatea SSO';
+$string['loginmoodle'] = 'Login clásico';
+$string['backtoselection'] = 'Volver a opciones de acceso';
+$string['loginlayout'] = 'Diseno de la página de login';
+$string['loginlayout_desc'] = 'Elige entre el login modificado o el login predeterminado de Moove.';
+$string['loginlayout_modified'] = 'Login modificado';
+$string['loginlayout_base'] = 'Login predeterminado';
+$string['identityprovidersmode'] = 'Visibilidad de proveedores de identidad';
+$string['identityprovidersmode_desc'] = 'Elige cómo se muestran los botones de proveedores de identidad en la página de acceso.';
+$string['identityprovidersmode_none'] = 'Ocultar todos los proveedores';
+$string['identityprovidersmode_single'] = 'Mostrar solo un proveedor';
+$string['identityprovidersmode_all'] = 'Mostrar todos los proveedores';
+$string['identityproviderssingle'] = 'Proveedor de identidad';
+$string['identityproviderssingle_desc'] = 'Selecciona qué proveedor se muestra al usar el modo "Mostrar solo un proveedor".';
+$string['identityproviderschoose'] = 'Selecciona un proveedor';
+$string['identityproviderschoosebutton'] = 'Opciones de OAuth';
